03a61bd165d8a1d346fd5f8645ad7621f365f266
Eric Lahoda <elahoda@gmail.com>
2023-08-23 12:42:26 -0400

    Mongoose Population

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
+
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
@@ -12,14 +12,11 @@ const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
 
 const passport = require("passport");
-const authenticate = require("./authenticate");
-
-const session = require("express-session");
-const FileStore = require("session-file-store")(session);
+const config = require("./config");
 
 const mongoose = require("mongoose");
 
-const url = "mongodb://127.0.0.1:27017/nucampsite";
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
   useFindAndModify: false,
@@ -44,34 +41,10 @@ app.use(express.urlencoded({ extended: false }));
 
 // app.use(cookieParser("12345-67890-09876-54321"));
 
-app.use(
-  session({
-    name: "session-id",
-    secret: "12345-67890-09876-54321",
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore(),
-  })
-);
-
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
-function auth(req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-    const err = new Error("You are not authenticated!");
-    err.status = 401;
-    return next(err);
-  } else {
-    return next();
-  }
-}
-app.use(auth);
 
 app.use(express.static(path.join(__dirname, "public")));
 
--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -1,75 +1,84 @@
-const express = require('express');
-const Promotion = require('../models/promotion');
+const express = require("express");
+const Promotion = require("../models/promotion");
+const authenticate = require("../authenticate");
 
 const promotionRouter = express.Router();
 
-promotionRouter.route('/')
-.get((req, res, next) => {
+promotionRouter
+  .route("/")
+  .get((req, res, next) => {
     Promotion.find()
-    .then(promotions => {
+      .then((promotions) => {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
+        res.setHeader("Content-Type", "application/json");
         res.json(promotions);
-    })
-    .catch(err => next(err));
-})
-.post((req, res, next) => {
+      })
+      .catch((err) => next(err));
+  })
+  .post(authenticate.verifyUser, (req, res, next) => {
     Promotion.create(req.body)
-    .then(promotion => {
-        console.log('Promotion Created ', promotion);
+      .then((promotion) => {
+        console.log("Promotion Created ", promotion);
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
+        res.setHeader("Content-Type", "application/json");
         res.json(promotion);
-    })
-    .catch(err => next(err));
-})
-.put((req, res) => {
+      })
+      .catch((err) => next(err));
+  })
+  .put(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
-    res.end('PUT operation not supported on /promotions');
-})
-.delete((req, res, next) => {
+    res.end("PUT operation not supported on /promotions");
+  })
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Promotion.deleteMany()
-    .then(response => {
+      .then((response) => {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
+        res.setHeader("Content-Type", "application/json");
         res.json(response);
-    })
-    .catch(err => next(err));
-});
+      })
+      .catch((err) => next(err));
+  });
 
-promotionRouter.route('/:promotionId')
-.get((req, res, next) => {
+promotionRouter
+  .route("/:promotionId")
+  .get((req, res, next) => {
     Promotion.findById(req.params.promotionId)
-    .then(promotion => {
+      .then((promotion) => {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
+        res.setHeader("Content-Type", "application/json");
         res.json(promotion);
-    })
-    .catch(err => next(err));
-})
-.post((req, res) => {
+      })
+      .catch((err) => next(err));
+  })
+  .post(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
-    res.end(`POST operation not supported on /promotions/${req.params.promotionId}`);
-})
-.put((req, res, next) => {
-    Promotion.findByIdAndUpdate(req.params.promotionId, {
-        $set: req.body
-    }, { new: true })
-    .then(promotion => {
+    res.end(
+      `POST operation not supported on /promotions/${req.params.promotionId}`
+    );
+  })
+  .put(authenticate.verifyUser, (req, res, next) => {
+    Promotion.findByIdAndUpdate(
+      req.params.promotionId,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    )
+      .then((promotion) => {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
+        res.setHeader("Content-Type", "application/json");
         res.json(promotion);
-    })
-    .catch(err => next(err));
-})
-.delete((req, res, next) => {
+      })
+      .catch((err) => next(err));
+  })
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Promotion.findByIdAndDelete(req.params.promotionId)
-    .then(response => {
+      .then((response) => {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
+        res.setHeader("Content-Type", "application/json");
         res.json(response);
-    })
-    .catch(err => next(err));
-});
+      })
+      .catch((err) => next(err));
+  });
 
 module.exports = promotionRouter;
